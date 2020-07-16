11e34b59ff4ccc5f1e10ea02b42080328a21b280
boggercell <boggercell@Boggercell-PC>
2020-07-16 16:44:06 +0630

    First Upload

---
+++ UniManagement/src/main/java/com/ideanet/unimanagement/UniManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ideanet.unimanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniManagementApplication.class, args);
+	}
+
+}
---
+++ UniManagement/src/test/java/com/ideanet/unimanagement/UniManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideanet.unimanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
